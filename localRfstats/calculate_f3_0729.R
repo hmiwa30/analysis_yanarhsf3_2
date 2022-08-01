6dbb25fb26d4dead9d8f96eaf4bf282413ecfeb4
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-01 22:54:49 +0900

    Update calculate_f3_0729.R

--- localRfstats/calculate_f3_0729.R
+++ localRfstats/calculate_f3_0729.R
@@ -35,25 +35,6 @@ my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1",rdir)
 extract_f2(prefix, my_f2_dir)
 
 
-#denisova
-sdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\sources"
-rdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\results"
-ldir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3"
-
-(prefix = sprintf("%s\\yana_j_f3_analysis_denisova",sdir))
-(my_f2_dir = sprintf("%s\\yana_j_f3_analysis_denisova",rdir))
-extract_f2(prefix, my_f2_dir)
-
-f2_blocks = f2_from_precomp(my_f2_dir)
-dim(f2_blocks)
-(lista <- read.table(sprintf("%s\\lista_yanajomon.txt",ldir))$V1)
-(pop1 = as.character(lista[1]))
-(pop2 = as.character(lista[9]))
-(pop3 = as.character(lista[-1]))
-dat.f3=f3(f2_blocks,pop1,pop2,pop3)
-write.table(dat.f3,sprintf("%s\\yana_j_f3_analysis_denisova.f3_FIN.txt",rdir),quote=F,row.names=F)
-
-
 
 #prefix = sprintf("G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3_2\\localRfstats\\sources",ah)
 #my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\output_%s",ah)
@@ -66,5 +47,7 @@ write.table(dat.f3,sprintf("%s\\yana_j_f3_analysis_denisova.f3_FIN.txt",rdir),qu
 #(pop3 = as.character(lista[-1]))
 #dat.f3=f3(f2_blocks,pop1,pop2,pop3)
 #write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\f3_FIN_%s.txt",ah),quote=F,row.names=F)
+
+
 
 
