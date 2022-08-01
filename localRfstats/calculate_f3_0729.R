5e3f9b6241d1d2d37388cd2510c8886a031741d1
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-01 16:29:14 +0900

    [update] calculate_f3_0729

--- localRfstats/calculate_f3_0729.R
+++ localRfstats/calculate_f3_0729.R
@@ -13,15 +13,14 @@
 library(admixtools)
 
 
-prefix = sprintf("G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3_2\\localRfstats\\sources",ah)
-my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\output_%s",ah)
-extract_f2(prefix, my_f2_dir)
-f2_blocks = f2_from_precomp(my_f2_dir)
-dim(f2_blocks)
-lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
-(pop1 = as.character(lista[1]))
-(pop2 = as.character(lista[9]))
-(pop3 = as.character(lista[-1]))
-dat.f3=f3(f2_blocks,pop1,pop2,pop3)
-write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\f3_FIN_%s.txt",ah),
-            quote=F,row.names=F)
+#prefix = sprintf("G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3_2\\localRfstats\\sources",ah)
+#my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\output_%s",ah)
+#extract_f2(prefix, my_f2_dir)
+#f2_blocks = f2_from_precomp(my_f2_dir)
+#dim(f2_blocks)
+#lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
+#(pop1 = as.character(lista[1]))
+#(pop2 = as.character(lista[9]))
+#(pop3 = as.character(lista[-1]))
+#dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+#write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\f3_FIN_%s.txt",ah),quote=F,row.names=F)
