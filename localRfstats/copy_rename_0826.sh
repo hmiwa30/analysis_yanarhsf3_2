0b4827ba485fa57c02a59916271ec903692813ea
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-26 02:38:02 +0900

    Create copy_rename_0826.sh

---
+++ localRfstats/copy_rename_0826.sh
@@ -0,0 +1,91 @@
+#!/mnt/hdd/hmiwa
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1.mini02
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02_part1.snp;echo "yanarhs_part1"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02_part2.snp;echo "yanarhs_part2"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02_part3.snp;echo "yanarhs_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02_part4.snp;echo "yanarhs_part4"
+#yana_j_f3_analysis_ch1.mini02
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02_part1.snp;echo "yana_j_part1"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02_part2.snp;echo "yana_j_part2"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02_part3.snp;echo "yana_j_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02_part4.snp;echo "yana_j_part4"
+echo "finish:`date`"
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1.mini03
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03_part1.snp;echo "yanarhs_part1"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03_part2.snp;echo "yanarhs_part2"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03_part3.snp;echo "yanarhs_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03_part4.snp;echo "yanarhs_part4"
+#yana_j_f3_analysis_ch1.mini03
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03_part1.snp;echo "yana_j_part1"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03_part2.snp;echo "yana_j_part2"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03_part3.snp;echo "yana_j_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03_part4.snp;echo "yana_j_part4"
+echo "finish:`date`"
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1.mini04
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04_part1.snp;echo "yanarhs_part1"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04_part2.snp;echo "yanarhs_part2"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04_part3.snp;echo "yanarhs_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04_part4.snp;echo "yanarhs_part4"
+#yana_j_f3_analysis_ch1.mini04
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04_part1.snp;echo "yana_j_part1"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04_part2.snp;echo "yana_j_part2"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04_part3.snp;echo "yana_j_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04_part4.snp;echo "yana_j_part4"
+echo "finish:`date`"
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1.mini05
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05_part1.snp;echo "yanarhs_part1"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05_part2.snp;echo "yanarhs_part2"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05_part3.snp;echo "yanarhs_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05_part4.snp;echo "yanarhs_part4"
+#yana_j_f3_analysis_ch1.mini05
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05_part1.snp;echo "yana_j_part1"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05_part2.snp;echo "yana_j_part2"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05_part3.snp;echo "yana_j_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05_part4.snp;echo "yana_j_part4"
+echo "finish:`date`"
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1.mini02
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02_part3.1.snp;echo "yanarhs_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02_part3.2.snp;echo "yanarhs_part3"
+echo "finish:`date`"
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1.mini03
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03_part3.1.snp;echo "yanarhs_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03_part3.2.snp;echo "yanarhs_part3"
+echo "finish:`date`"
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1.mini04
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04_part3.1.snp;echo "yanarhs_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04_part3.2.snp;echo "yanarhs_part3"
+echo "finish:`date`"
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1.mini05
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05_part3.1.snp;echo "yanarhs_part3"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05.snp > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05_part3.2.snp;echo "yanarhs_part3"
+echo "finish:`date`"
+echo "start:`date`"
+#yanarhs_f3_analysis_ch1
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini02.ind;echo "yanarhs_mini02"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini03.ind;echo "yanarhs_mini03"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini04.ind;echo "yanarhs_mini04"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis_ch1.mini05.ind;echo "yanarhs_mini05"
+#yanarhs_f3_analysis2_ch1
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis2_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis2_ch1.mini02.ind;echo "yanarhs_mini02"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis2_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis2_ch1.mini03.ind;echo "yanarhs_mini03"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis2_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis2_ch1.mini04.ind;echo "yanarhs_mini04"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis2_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yanarhs_f3_analysis2_ch1.mini05.ind;echo "yanarhs_mini05"
+#yana_j_f3_analysis_ch1
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini02.ind;echo "yana_j_mini02"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini03.ind;echo "yana_j_mini03"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini04.ind;echo "yana_j_mini04"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis_ch1.mini05.ind;echo "yana_j_mini05"
+#yana_j_f3_analysis2_ch1
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis2_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis2_ch1.mini02.ind;echo "yana_j_mini02"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis2_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis2_ch1.mini03.ind;echo "yana_j_mini03"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis2_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis2_ch1.mini04.ind;echo "yana_j_mini04"
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis2_ch1.ind > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3_2/localRfstats/sources/yana_j_f3_analysis2_ch1.mini05.ind;echo "yana_j_mini05"
+echo "finish:`date`"
