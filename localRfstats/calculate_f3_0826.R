37f54144a54c0cac52cc2841cb44a49c298ff9c4
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-26 02:37:49 +0900

    Create calculate_f3_0826.R

---
+++ localRfstats/calculate_f3_0826.R
@@ -0,0 +1,417 @@
+.libPaths()
+.libPaths("C:\\Users\\himar\\Documents\\R")
+
+#install.packages("shiny")
+#install.packages("shinythemes")
+#install.packages("shinyjs")
+#install.packages("shinyBS")
+#install.packages("shinyFiles")
+#install.packages("shinyalert")
+#install.packages("shinyWidgets")
+#install.packages("shinydashboard")
+#install.packages("DT")
+#install.packages("jquerylib")
+library(admixtools)
+
+sdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3_2\\localRfstats\\sources"
+rdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3_2\\localRfstats\\results"
+ldir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3_2"
+
+gc(reset=T)
+gc(reset=T)
+
+# extract_f2 mini02####
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part1",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini02_part1",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini02_part1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part2",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini02_part2",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini02_part2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part3",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part3",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini02_part3",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini02_part3",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part4",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part4",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini02_part4",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini02_part4",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part3.1",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part3.1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part3.2",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini02_part3.2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+# extract_f2 mini03####
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part1",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini03_part1",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini03_part1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part2",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini03_part2",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini03_part2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part3",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part3",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini03_part3",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini03_part3",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part4",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part4",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini03_part4",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini03_part4",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part3.1",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part3.1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part3.2",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini03_part3.2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+# extract_f2 mini04####
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part1",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini04_part1",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini04_part1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part2",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini04_part2",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini04_part2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part3",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part3",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini04_part3",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini04_part3",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part4",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part4",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini04_part4",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini04_part4",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part3.1",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part3.1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part3.2",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini04_part3.2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+# extract_f2 mini05####
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part1",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini05_part1",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini05_part1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part2",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini05_part2",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini05_part2",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part3",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part3",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini05_part3",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini05_part3",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part4",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part4",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yana_j_f3_analysis_ch1.mini05_part4",sdir)
+my_f2_dir = sprintf("%s\\yana_j_f3_analysis_ch1.mini05_part4",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part3.1",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part3.1",rdir)
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+prefix = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part3.2",sdir)
+my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_ch1.mini05_part3.2",rdir)
+extract_f2(prefix, my_f2_dir)
